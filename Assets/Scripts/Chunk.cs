485b7e57d4b45f53e4878b9545d44c44823e535f
Sébastien Maes <sebastien.maes@protonmail.com>
2021-11-16 17:23:55 +0100

    Publish Project

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    int vertexIndex = 0;
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
+    byte[,,] voxelMap = new byte[VoxelData.CHUNK_WIDTH, VoxelData.CHUNK_HEIGHT, VoxelData.CHUNK_WIDTH];
+
+    World world;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        world = GameObject.Find("World").GetComponent<World>();
+
+        PopulateVoxelMap();
+        CreateMeshData();
+        CreateMesh();
+    }
+
+    void PopulateVoxelMap()
+    {
+        for (int y = 0; y < VoxelData.CHUNK_HEIGHT; y++)
+        {
+            for (int x = 0; x < VoxelData.CHUNK_WIDTH; x++)
+            {
+                for (int z = 0; z < VoxelData.CHUNK_WIDTH; z++)
+                {
+                    if (y < 1)
+                        voxelMap[x, y, z] = 7; // Bedrock
+                    else if (y == VoxelData.CHUNK_HEIGHT - 1)
+                        voxelMap[x, y, z] = 2; // Grass
+                    else
+                        voxelMap[x, y, z] = 0; // Stone
+                }
+            }
+        }
+    }
+
+    void CreateMeshData()
+    {
+        for (int y = 0; y < VoxelData.CHUNK_HEIGHT; y++)
+        {
+            for (int x = 0; x < VoxelData.CHUNK_WIDTH; x++)
+            {
+                for (int z = 0; z < VoxelData.CHUNK_WIDTH; z++)
+                {
+                    AddVoxelDataToChunk(new Vector3(x, y, z));
+                }
+            }
+        }
+    }
+
+    bool CheckVoxel(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        // If it's outside of the current chunk
+        if (x < 0 || x > VoxelData.CHUNK_WIDTH - 1 || y < 0 || y > VoxelData.CHUNK_HEIGHT - 1 || z < 0 || z > VoxelData.CHUNK_WIDTH - 1)
+            return false;
+
+        return world.blockTypes[voxelMap[x, y, z]].isSolid;
+    }
+
+    void AddVoxelDataToChunk(Vector3 pos)
+    {
+        // For each voxel face
+        for (int faceIdx = 0; faceIdx < VoxelData.NB_FACES; faceIdx++)
+        {
+            // If there is no voxel against that face
+            if (!CheckVoxel(pos + VoxelData.faceChecks[faceIdx]))
+            {
+                byte blockID = voxelMap[(int)pos.x, (int)pos.y, (int)pos.z];
+
+                // Render a face
+                /*for (int i = 0; i < VoxelData.NB_FACE_VERTICES; i++) 
+                {
+                    int triangleIndex = VoxelData.voxelTriangles[p, i];
+                    vertices.Add(VoxelData.voxelVertices[triangleIndex] + pos);
+                    triangles.Add(vertexIndex);
+
+                    uvs.Add(VoxelData.voxelUvs[i]);
+
+                    vertexIndex++;
+                }*/
+                vertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTriangles[faceIdx, 0]]);
+                vertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTriangles[faceIdx, 1]]);
+                vertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTriangles[faceIdx, 2]]);
+                vertices.Add(pos + VoxelData.voxelVertices[VoxelData.voxelTriangles[faceIdx, 3]]);
+
+                /*uvs.Add(VoxelData.voxelUvs[0]);
+                uvs.Add(VoxelData.voxelUvs[1]);
+                uvs.Add(VoxelData.voxelUvs[2]);
+                uvs.Add(VoxelData.voxelUvs[3]);*/
+                AddTexture(world.blockTypes[blockID].GetTextureID(faceIdx));
+
+                triangles.Add(vertexIndex);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
+
+                vertexIndex += 4;
+            }
+        }
+    }
+
+    void CreateMesh()
+    {
+        Mesh mesh = new Mesh
+        {
+            vertices = vertices.ToArray(),
+            triangles = triangles.ToArray(),
+            uv = uvs.ToArray()
+        };
+
+        mesh.RecalculateNormals();
+        meshFilter.mesh = mesh;
+    }
+
+    void AddTexture(int textureID)
+    {
+        float y = textureID / VoxelData.TextureAtlasSizeInBlocks;
+        float x = textureID - (y * VoxelData.TextureAtlasSizeInBlocks);
+
+        x *= VoxelData.NormalizedBlockTextureSize;
+        y *= VoxelData.NormalizedBlockTextureSize;
+
+        y = 1f - y - VoxelData.NormalizedBlockTextureSize;
+
+        uvs.Add(new Vector2(x, y));
+        uvs.Add(new Vector2(x, y + VoxelData.NormalizedBlockTextureSize));
+        uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y));
+        uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y + VoxelData.NormalizedBlockTextureSize));
+    }
+}
---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VoxelData
+{
+    public static readonly int NB_FACE_VERTICES = 6; // 2 triangles
+    public static readonly int NB_FACES = 6; // because it's a cube
+    public static readonly int CHUNK_WIDTH = 16;
+    public static readonly int CHUNK_HEIGHT = 128;
+
+    public static readonly int TextureAtlasSizeInBlocks = 32;
+    public static float NormalizedBlockTextureSize
+    {
+        get
+        {
+            return 1f / (float)TextureAtlasSizeInBlocks;
+        }
+    }
+
+    // Vertices defining a voxel (cube)
+    public static readonly Vector3[] voxelVertices = new Vector3[]
+    {
+        new Vector3(0.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 1.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 1.0f)
+    };
+
+    // Defines faces positions to check for rendering
+    public static readonly Vector3[] faceChecks = new Vector3[]
+    {
+        new Vector3(0.0f, 0.0f, -1.0f), // Back Face
+        new Vector3(0.0f, 0.0f, 1.0f), // Front Face
+        new Vector3(0.0f, 1.0f, 0.0f), // Top Face
+        new Vector3(0.0f, -1.0f, 0.0f), // Bottom Face
+        new Vector3(-1.0f, 0.0f, 0.0f), // Left Face
+        new Vector3(1.0f, 0.0f, 0.0f) // Right Face
+    };
+
+    // Defines each face with vertex indexes defining triangles (2 triangles per face)
+    public static readonly int[,] voxelTriangles = 
+    {
+        // Back, Front, Top, Bottom, Left, Right
+
+        // 0 3 1 1 3 2
+        {0, 3, 1, 2}, // Back Face
+        {5, 6, 4, 7}, // Front Face
+        {3, 7, 2, 6}, // Top Face
+        {1, 5, 0, 4}, // Bottom Face
+        {4, 7, 0, 3}, // Left Face
+        {1, 2, 5, 6}  // Right Face
+    };
+
+    // Map texture coordinates on the face of a voxel
+    public static readonly Vector2[] voxelUvs = new Vector2[]
+    {
+        new Vector2(0.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(1.0f, 1.0f)
+    };
+}
